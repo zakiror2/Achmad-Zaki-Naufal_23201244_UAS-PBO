3d980d51dbca9e9c5468173096d34595c17f216a
C-2 <C-2@C-2>
2024-07-20 10:48:23 +0700

    uas pbo

---
+++ src/main/java/com/mycompany/uaspbonoteapp/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uaspbonoteapp;
+
+
+import com.mycompany.uaspbonoteapp.db.NoteAppMenu;
+
+
+/**
+ *2
+ * @author budi
+ */
+public class Main{
+
+    public static void main(String[] args) {
+        NoteAppMenu noteapp = new NoteAppMenu("notes.db");
+        noteapp.start();
+    }
+}
